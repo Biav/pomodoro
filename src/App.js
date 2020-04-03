4a60c2e696169a6e89dc1852cdc64eceac662a38
Bia <beatrizvieirasilva.v@gmail.com>
2020-04-03 17:22:07 -0300

    Menu Component

--- src/App.js
+++ src/App.js
@@ -1,12 +1,19 @@
-import React from 'react';
+import React, { Component } from 'react';
+import { Route } from 'react-router-dom';
+import 'bootstrap/dist/css/bootstrap.css';
+import 'bootstrap/dist/js/bootstrap.bundle.min';
 import './App.css';
+import home from './components/Layout/Home/Home';
 
-function App() {
-  return (
-    <div className="App">
-      Pomodoro
-    </div>
-  );
+class App extends Component {
+  render(){
+    return (
+      <div className="App">
+        <Route path="/" component = { home }/>
+      </div>
+    )
+  };
+
 }
 
 export default App;
